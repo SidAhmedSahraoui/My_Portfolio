e111de7880d9c60b30fc17d9ec92d1de93d743c0
sid ahmed <s.sahraoui@esi-sba.dz>
2021-11-20 18:30:47 +0100

    1

---
+++ build/precache-manifest.bc89b307f05abe3e7a88eba1ecd5c4b3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581618402be29bafa777796ac8065de8",
+    "url": "/My_Portfolio/index.html"
+  },
+  {
+    "revision": "fa6f74d857c1c7e71e81",
+    "url": "/My_Portfolio/static/css/main.2445e656.chunk.css"
+  },
+  {
+    "revision": "ec057294ebb80ab05126",
+    "url": "/My_Portfolio/static/js/2.cb403d30.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "/My_Portfolio/static/js/2.cb403d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6f74d857c1c7e71e81",
+    "url": "/My_Portfolio/static/js/main.ee9d15cf.chunk.js"
+  },
+  {
+    "revision": "1e2c103d3d0dbe3d8cb1",
+    "url": "/My_Portfolio/static/js/runtime-main.1f06d715.js"
+  },
+  {
+    "revision": "07d9a5adbb3aee2d3b125c265dc0ac27",
+    "url": "/My_Portfolio/static/media/PSX_2021.07d9a5ad.jpg"
+  },
+  {
+    "revision": "ada02541b37b7d4b2bebe198f576d239",
+    "url": "/My_Portfolio/static/media/PXL_2021.ada02541.jpg"
+  },
+  {
+    "revision": "71140e961cdcb6e13d2580eb937ec90a",
+    "url": "/My_Portfolio/static/media/android.71140e96.png"
+  },
+  {
+    "revision": "d8d343097640f498d679947ac98e9ef4",
+    "url": "/My_Portfolio/static/media/claim.d8d34309.png"
+  },
+  {
+    "revision": "e3e894ccfa725bc4f97a1bd68abe1fdb",
+    "url": "/My_Portfolio/static/media/icsi.e3e894cc.png"
+  },
+  {
+    "revision": "d060c014216d37624e66c02a3516d429",
+    "url": "/My_Portfolio/static/media/noty.d060c014.png"
+  },
+  {
+    "revision": "d16f59531a3cd41feab39c383e2e31c1",
+    "url": "/My_Portfolio/static/media/soc.d16f5953.png"
+  }
+]);
